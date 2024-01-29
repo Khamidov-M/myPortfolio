3ff5a0d7f47eba1cd56b0571f6289c4d1d976b40
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-01-29 09:42:32 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import { experiences } from "./constants";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <Works />
+        <About />
+        {experiences.length > 2 && <Experience />}
+        <Tech />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
